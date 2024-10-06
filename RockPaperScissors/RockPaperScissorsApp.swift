a8b7d61c2dfe52abe82d38f7f70cfaa2186d724a
Razvan Pricop <razvanipricop@gmail.com>
2024-10-07 01:01:25 +0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Razvan Pricop on 07.10.24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
